2583855fa8afd8829513a5188eba75cba59d0522
bpourian <b.pourian@hotmail.co.uk>
2018-07-05 15:26:42 +0100

    Added additional subnet12

--- main.tf
+++ main.tf
@@ -17,8 +17,15 @@ resource "azurerm_resource_group" "AZBENGROUPVN" {
 }
 
 resource "azurerm_subnet" "AZBENSUB11" {
-  name                 = "AZBENSUB"
+  name                 = "AZBENSUB11"
   resource_group_name  = "${azurerm_resource_group.AZBENGROUPVN.name}"
   virtual_network_name = "${azurerm_virtual_network.AZBENVN1.name}"
   address_prefix       = "10.0.0.0/24"
+}
+
+resource "azurerm_subnet" "AZBENSUB12" {
+  name                 = "AZBENSUB12"
+  resource_group_name  = "${azurerm_resource_group.AZBENGROUPVN.name}"
+  virtual_network_name = "${azurerm_virtual_network.AZBENVN1.name}"
+  address_prefix       = "10.0.1.0/24"
 }
