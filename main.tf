e21a0ea1fbad79a0035a6ee9e54d96d0a308814b
bpourian <b.pourian@hotmail.co.uk>
2018-07-05 15:23:18 +0100

    Created resource for VN and subnet11

--- main.tf
+++ main.tf
@@ -9,9 +9,16 @@ resource "azurerm_resource_group" "AZBENGROUPVN" {
 }
 
 
- resource "azurerm_virtual_network" "AZBENVN" {
+ resource "azurerm_virtual_network" "AZBENVN1" {
   name                = "AZBENVN"
   address_space       = ["10.0.0.0/23"]
   location            = "UK West"
   resource_group_name = "${azurerm_resource_group.AZBENGROUPVN.name}"
+}
+
+resource "azurerm_subnet" "AZBENSUB11" {
+  name                 = "AZBENSUB"
+  resource_group_name  = "${azurerm_resource_group.AZBENGROUPVN.name}"
+  virtual_network_name = "${azurerm_virtual_network.AZBENVN1.name}"
+  address_prefix       = "10.0.0.0/24"
 }
