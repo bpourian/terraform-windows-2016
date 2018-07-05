09e83fb7007392ea5cafe663fc1b12fe47ea7bc9
bpourian <b.pourian@hotmail.co.uk>
2018-07-05 16:20:01 +0100

    Created public ip resource and network interface

--- main.tf
+++ main.tf
@@ -29,3 +29,24 @@ resource "azurerm_subnet" "AZBENSUB12" {
   virtual_network_name = "${azurerm_virtual_network.AZBENVN1.name}"
   address_prefix       = "10.0.1.0/24"
 }
+
+resource "azurerm_public_ip" "AZBENIP" {
+  name                         = "AZBENIP"
+  location                     = "UK West"
+  resource_group_name          = "${azurerm_resource_group.AZBENGROUP.name}"
+  public_ip_address_allocation = "dynamic"
+}
+
+resource "azurerm_network_interface" "AZBENNI-11" {
+  name                = "AZBENNI-11"
+  location            = "UK West"
+  resource_group_name = "${azurerm_resource_group.AZBENGROUPVN.name}"
+
+  ip_configuration {
+    name                          = "AZBENVNIP1"
+    subnet_id                     = "${azurerm_subnet.AZBENSUB11.id}"
+    private_ip_address_allocation = "static"
+    private_ip_address            = "10.0.0.4"
+    public_ip_address_id          = "${azurerm_public_ip.AZBENIP.id}"
+  }
+}
